71b9a4cf0969a0dcdc94735c4db47bd96154685f
mukae9 <rockjoint2016@gmail.com>
2020-01-24 03:49:32 +0900

    first

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use App\User;
+use Auth;
+use Socialite;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    public function redirectToTwitterProvider()
+    {
+        return Socialite::driver('twitter')->redirect();
+    }
+
+    public function handleTwitterProviderCallback(){
+
+        try {
+            $user = Socialite::with("twitter")->user();
+        } 
+        catch (\Exception $e) {
+            return redirect('/login')->with('oauth_error', 'ログインに失敗しました');
+            // エラーならログイン画面へ転送
+        }
+        
+        $myinfo = User::firstOrCreate(['token' => $user->token ],
+                  ['name' => $user->nickname,'email' => $user->getEmail()]);
+                  Auth::login($myinfo);
+                  return redirect()->to('/'); // homeへ転送
+     
+     }
+}
